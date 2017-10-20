ce620df26824da7632f95e4885bcc931d5fdb67f
José Eduardo Vieira Morango <edvmorango@gmail.cm>
2017-10-20 19:50:16 +0000

    Added VolatileVar

---
+++ src/main/scala/Volatile.scala
@@ -0,0 +1,27 @@
+import java.time.LocalDateTime
+
+object VolatileVar extends App{
+  case class Invoice(identifier: Long, store: String, product: String)
+
+  @volatile var ready = false
+
+  val threads = (0 until 5).map { _ =>
+    ThreadProducer.thread {
+      while(!ready){}
+
+      Thread.sleep(1000)
+      println(s"Finishing Thread ${Thread.currentThread().getName} -- ${LocalDateTime.now()}")
+    }
+  }
+
+  Thread.sleep(1000)
+  threads.foreach(t => println(s"${t.getName} --- ${t.getState}"))
+  ready = true
+
+}
+
+
+
+
+
+
