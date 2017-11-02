10819b4d4289cce34d79a665368072db06806133
J. Eduardo V. Morango <edvmorango@gmail.com>
2017-11-02 15:59:19 -0200

    Future continuation

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ http://rerun.me/2016/06/05/scala-notes-futures-2-promises/
 https://stackoverflow.com/questions/18960339/clarification-needed-about-futures-and-promises-in-scala
 
 http://robotlolita.me/2015/11/15/how-do-promises-work.html#21-what-are-promises
+
+http://pchiusano.blogspot.com.br/2010/01/actors-are-not-good-concurrency-model.html
--- src/main/scala/FuturesAndPromises/FuturesAndPromises.scala
+++ src/main/scala/FuturesAndPromises/FuturesAndPromises.scala
@@ -1,8 +1,10 @@
 package FuturesAndPromises
 
+import java.time.LocalDateTime
+
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 import scala.util.control.NonFatal
 
 object FuturesExecution extends App {
@@ -50,22 +52,68 @@ object FuturesCallback extends App {
 object FuturesFailure extends App {
 
   val f: Future[Double] = Future { 10/0 }
+  /*
+  val f = Future { throw new InterruptedException }
+  */
 
   f.foreach{v => println(s"Result: $v")}
 
   f.failed.foreach{e => println(s"Exception: $e")}
 
-  Thread.sleep(10000)
+  Thread.sleep(2000)
 }
 
 object FuturesFailuresTry extends App {
 
-  val f = Future { throw new InterruptedException }
+  val f: Future[Int] = Future { 10/0 }
 
-  f.failed.foreach{
-    case NonFatal(e) => println(s"Exception: $e")
+  f.onComplete { _ match {
+    case Success(v) => println(s"Value: $v")
+    case Failure(e) => println(s"Exception: $e")
+    }
   }
 
   Thread.sleep(2000)
+}
 
+object FuturesNesting extends App {
+
+  case class Token(value: String, expirationDate: LocalDateTime) {
+    def isValid = { !value.isEmpty && expirationDate.compareTo(LocalDateTime.now()) >= 0 }
+  }
+
+  def getInfo(token: Token): List[String] = {
+    println(s"getInfo: ${Thread.currentThread().getName}")
+    if(token.isValid)
+      List("info1", "info2", "info3")
+    else
+      Nil
+  }
+
+  def getMoreInfo(token: Token, list: List[String]) = {
+    println(s"getMoreInfo: ${Thread.currentThread().getName}")
+    println("More info")
+  }
+
+  val f: Future[Token] = Future { Token("token", LocalDateTime.MAX) }
+
+  f.foreach { token =>
+
+    println(s"f Future: ${Thread.currentThread().getName}")
+
+    val info = getInfo(token)
+
+    Future { getInfo(token) }.foreach { list =>
+
+      println(s"getInfo Future: ${Thread.currentThread().getName}")
+
+      getMoreInfo(token, list)
+    }
+
+    println(info)
+  }
+
+  println(s"Main: ${Thread.currentThread().getName}")
+
+  Thread.sleep(2000)
 }
